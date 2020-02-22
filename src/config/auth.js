00c772ec04455e244ea30dc4e5a5717779e35ea9
Diego Collares <diegocollares@brq.com>
2020-02-22 20:41:02 -0300

    created login with jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'bb4f6b6babc0c14f67e99f4601aaaaca',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', UserController.all);
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
