fd89cb9bb36ee24536203aad2dab0b5bf1edaada
Diego Collares <diegocollares@brq.com>
2020-02-22 21:56:17 -0300

    config middleware auth

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -6,7 +6,7 @@ import authConfig from '../../config/auth';
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (authHeader) {
+  if (!authHeader) {
     res.status(401).json({ error: 'token not provided' });
   }
 
@@ -14,7 +14,6 @@ export default async (req, res, next) => {
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    console.log(decoded);
     req.userId = decoded.id;
     return next();
   } catch (error) {
