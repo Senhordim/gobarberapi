07127a343ca2f0933fe4cac34443512874861c83
Diego Collares <diegocollares@brq.com>
2020-02-26 10:52:12 -0300

    config Sentry

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
+import 'express-async-errors';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +29,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://db6f6a31d9c149aab3026839a1c486d5@sentry.io/2976936',
+};
